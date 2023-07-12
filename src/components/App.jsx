5f69c6381d9effdcf28169300f9806f2767b5940
Anastasiia <schili@outlook.com>
2023-07-12 19:02:26 +0300

    markup of first card

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,8 @@
+import OutlinedCard from "./Card";
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <OutlinedCard />
   );
 };
